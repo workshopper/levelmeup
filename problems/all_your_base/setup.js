20f10b2008d17fc79f92073073cec67be0bcd2fc
Rod Vagg <rod@vagg.org>
2013-09-02 21:00:33 +1000

    added hello world / all your base starter problem

---
+++ problems/all_your_base/setup.js
@@ -0,0 +1,11 @@
+// array for substitution in: 'ALL YOUR X ARE BELONG TO Y' where 
+const inputs = [
+    [ 'BASE', 'US' ]
+  , [ 'LANDS AND POSESSIONS', 'VIKINGS OF WESTERN NORWAY' ]
+  , [ 'INDEPENDENCE', 'KING HENRY VIII' ]
+]
+
+module.exports = function () {
+  var i = Math.floor(Math.random() * inputs.length)
+  return { args: inputs[i], stdin: null }
+}
