4a76711b8ef47c2f0539bba930f99d00798f46c7
Rod Vagg <rod@vagg.org>
2013-09-09 03:40:07 +1000

    cleaned up streaming exercise

--- problems/streaming/setup.js
+++ problems/streaming/setup.js
@@ -1,23 +1,11 @@
- const path  = require('path')
-    , fs     = require('fs')
-    , os     = require('os')
-    , level  = require('level')
-    , rimraf = require('rimraf')
-    , after  = require('after')
-    , dir1   = path.join(os.tmpDir(), '~levelmeup_1_' + process.pid)
-    , dir2   = path.join(os.tmpDir(), '~levelmeup_2_' + process.pid)
-
-function cleanup () {
-  dir1 && rimraf.sync(dir1, function () {})
-  dir2 && rimraf.sync(dir2, function () {})
-}
+const path     = require('path')
+    , fs       = require('fs')
+    , existing = require('../../lib/setup-existing')
 
 function setup (run, callback) {
-  cleanup()
+  existing.setup(run)
 
-  var db1 = level(dir1)
-    , db2 = !run && level(dir2)
-    , ops = fs.readFileSync(path.join(__dirname, '../../data/node_0.8_commits.dat'), 'utf8')
+  var ops = fs.readFileSync(path.join(__dirname, '../../data/node_0.8_commits.dat'), 'utf8')
         .split('\n')
         .map(function (line) {
           var m = line.match(/^([^\s]+)\s+(.+)$/)
@@ -25,26 +13,21 @@ function setup (run, callback) {
         })
         .filter(Boolean)
 
-  var done = after(run ? 1 : 2, function (err) {
-  if (err)
-    return callback(err)
-
-    callback(null, {
-        submissionArgs   : [ dir1 ]
-      , solutionArgs     : [ dir2 ]
-      , long             : true
-      , close            : cleanup
-    })
-  })
-
-  function close (err) {
-    if (err)
-      return done(err)
-    this.close(done)
-  }
-
-  db1.batch(ops, close.bind(db1))
-  ;!run && db2.batch(ops, close.bind(db2))
+  existing.writeAndClose(
+      function (db, callback) {
+        db.batch(ops, callback)
+      }
+    , function (err) {
+        if (err)
+          return callback(err)
+        callback(null, {
+            submissionArgs : [ existing.dir1 ]
+          , solutionArgs   : [ existing.dir2 ]
+          , long           : true
+          , close          : existing.cleanup
+        })
+      }
+  )
 }
 
 module.exports       = setup
