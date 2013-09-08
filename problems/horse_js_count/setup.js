a7024a4b7d37c7fe6f3ae9a03e0ae4b0187cdfe8
Rod Vagg <rod@vagg.org>
2013-09-09 04:39:22 +1000

    horse_js counts cleanup

--- problems/horse_js_count/setup.js
+++ problems/horse_js_count/setup.js
@@ -1,53 +1,39 @@
-const horsejs     = require('../../data/horse_js.json')
-    , path        = require('path')
-    , fs          = require('fs')
-    , os          = require('os')
-    , level       = require('level')
-    , rimraf      = require('rimraf')
-    , after       = require('after')
-    , dir1        = path.join(os.tmpDir(), '~levelmeup_1_' + process.pid)
-    , dir2        = path.join(os.tmpDir(), '~levelmeup_2_' + process.pid)
-
-function cleanup () {
-  dir1 && rimraf.sync(dir1, function () {})
-  dir2 && rimraf.sync(dir2, function () {})
-}
+const path     = require('path')
+    , existing = require('../../lib/setup-existing')
+    , horsejs  = require('../../data/horse_js.json')
 
 function setup (run, callback) {
-  cleanup()
+  existing.setup(run)
 
-  var db1 = level(dir1)
-    , db2 = !run && level(dir2)
-    , ops = horsejs.map(function (tweet) {
-        return {
-            type  : 'put'
-          , key   : new Date(tweet.date).toISOString()
-          , value : tweet.tweet
-        }
-      })
-
-  var done = after(run ? 1 : 2, function (err) {
-  if (err)
-    return callback(err)
-
-    callback(null, {
-        submissionArgs   : [ dir1 ]
-      , solutionArgs     : [ dir2 ]
-      , long             : true
-      , close            : cleanup
-      , execWrap         : require.resolve('./exec-wrap')
-      , solutionExecWrap : require.resolve('./exec-wrap')
-    })
+  var ops = horsejs.map(function (tweet) {
+    return {
+        type  : 'put'
+      , key   : new Date(tweet.date).toISOString()
+      , value : tweet.tweet
+    }
   })
 
-  function close (err) {
-    if (err)
-      return done(err)
-    this.close(done)
-  }
+  var start = new Date(horsejs[0].date)
+    , end   = new Date(horsejs[horsejs.length - 1].date)
+    , mark  = new Date(start.getTime() + Math.floor(Math.random() * (end.getTime() - start.getTime())))
+    , marks = mark.getFullYear() + '-' + (mark.getUTCMonth() < 9 ? '0' : '') + (mark.getUTCMonth() + 1) + '-' + (mark.getUTCDate() < 9 ? '0' : '') + (mark.getUTCDate() + 1)
 
-  db1.batch(ops, close.bind(db1))
-  ;!run && db2.batch(ops, close.bind(db2))
+  existing.writeAndClose(
+      function (db, callback) {
+        db.batch(ops, callback)
+      }
+    , function (err) {
+        if (err)
+          return callback(err)
+
+        callback(null, {
+            submissionArgs : [ existing.dir1, marks ]
+          , solutionArgs   : [ existing.dir2, marks ]
+          , long           : true
+          , close          : existing.cleanup
+        })
+      }
+  )
 }
 
 module.exports       = setup
--- problems/horse_js_count/solution.js
+++ problems/horse_js_count/solution.js
@@ -1,15 +1,11 @@
-module.exports = function (db, day, callback) {
-  var tweets = []
-  db.createReadStream({ start: day, end: day + '\xff' })
-    .on('data', function (data) {
-      tweets.push(data.value)
-    })
-    .on('error', function (err) {
-      if (callback) callback(err)
-      callback = null
-    })
-    .on('end', function () {
-      if (callback) callback(null, tweets)
-      callback = null
-    })
-}
+var level = require('level')
+var db = level(process.argv[2])
+var tweets = 0
+
+db.createReadStream({ start: process.argv[3] })
+  .on('data', function (data) {
+    tweets++
+  })
+  .on('end', function () {
+    console.log(tweets)
+  })
