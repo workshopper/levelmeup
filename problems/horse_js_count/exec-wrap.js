6d0eab46ad1e3bccb528dbc8a817d12378fe070d
Rod Vagg <rod@vagg.org>
2013-09-09 20:42:35 +1000

    fix & tweak @horse_js exercise

--- problems/horse_js_count/exec-wrap.js
+++ problems/horse_js_count/exec-wrap.js
@@ -2,26 +2,27 @@
 module.exports.init = function () {}
 module.exports.args = -1
 
-const path  = require('path')
-    , level = require('level')
-    , db    = level(process.argv[4])
-    , file  = process.argv[3]
-
-var solution = require(path.resolve(process.cwd(), file))
+const path     = require('path')
+    , level    = require('level')
+    , db       = level(process.argv[4])
+    , file     = process.argv[3]
+    , horsejs  = require('../../data/horse_js.json')
+    , solution = require(path.resolve(process.cwd(), file))
 
 if (typeof solution != 'function')
   return console.log(process.argv[3], 'does not export a single function')
 
-solution(db, '2013-06-01', function (err, tweets) {
+solution(db, process.argv[4], function (err, count) {
   if (err) throw err
-  tweets.forEach(function (tweet) {
-    console.log('<@horse_js 2013-06-01>', tweet)
-  })
+  console.log('@horse_js tweets since', process.argv[4] + ':', count)
 
-  solution(db, '2013-07-11', function (err, tweets) {
+  solution(db, process.argv[5], function (err, count) {
     if (err) throw err
-    tweets.forEach(function (tweet) {
-      console.log('<@horse_js 2013-07-11>', tweet)
+    console.log('@horse_js tweets since', process.argv[5] + ':', count)
+
+    solution(db, process.argv[6], function (err, count) {
+      if (err) throw err
+      console.log('@horse_js tweets since', process.argv[6] + ':', count)
     })
   })
 })
