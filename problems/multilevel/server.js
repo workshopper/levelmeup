3f980956b13c0b90acc0e4f14833918a1db02c1f
Julian Gruber <julian@juliangruber.com>
2013-09-09 16:37:35 +0100

    make server listen

--- problems/multilevel/server.js
+++ problems/multilevel/server.js
@@ -6,6 +6,6 @@ var db = level(__dirname + '/db');
 
 net.createServer(function(con) {
   con.pipe(multilevel.server(db)).pipe(con);
-});
+}).listen(4545);
 
 db.put('the holy secret', 'the solution is *you*');
