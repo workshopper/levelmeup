2e61ff74740b181af6ae6c0e2482d6ebfce462d1
Martin Heidegger <martin.heidegger@gmail.com>
2015-11-07 19:05:45 +0900

    no message

---
+++ exercises/all_your_base/problem.md
@@ -0,0 +1,17 @@
+To get into the rhythm, let's do a simple _hello world_.
+
+Write a Node program that accepts two command-line arguments, **X** and **Y** and prints out to the console the following text:
+
+ALL YOUR **X** ARE BELONG TO **Y**
+
+When you have completed your program, you can run it in the test
+environment with:
+
+    {appname} run program.js
+
+And once you are happy that it is correct then run:
+
+    {appname} verify program.js
+
+And your submission will be verified for correctness. After you have
+a correct solution, run **`{appname}`** again and select the next problem!
--- levelmeup.js
+++ levelmeup.js
@@ -1,16 +1,31 @@
 #!/usr/bin/env node
 
-const workshopper = require('workshopper')
+const workshopper = require('workshopper-adventure')
     , path        = require('path')
+    , levelMeUp   = workshopper({
+        title        : 'LEVEL ME UP SCOTTY!'
+      , subtitle     : 'Learn You Some Node.js Databases'
+      , appDir       : __dirname
+      , menu         : {
+            bg : 'green'
+        }
+      , header      : require('workshopper-adventure/default/header')
+      , helpFile : path.join(__dirname, 'help.txt')
+    })
 
-workshopper({
-    name         : 'levelmeup'
-  , title        : 'LEVEL ME UP SCOTTY!'
-  , subtitle     : 'Learn You Some Node.js Databases'
-  , exerciseDir : path.join(__dirname, './exercises/')
-  , appDir       : __dirname
-  , menu         : {
-        bg : 'green'
-    }
-  , helpFile : path.join(__dirname, 'help.txt')
-})//.init()
+levelMeUp.addAll([
+  "ALL YOUR_BASE",
+  "BASICS BATCH",
+  "BASICS GET",
+  "BASICS PUT",
+  "GET YOUR LEVEL ON",
+  "HORSE JS COUNT",
+  "HORSE JS TWEETS",
+  "KEYWISE",
+  "MULTILEVEL",
+  "SHORT SCRABBLE WORDS",
+  "STREAMING",
+  "SUBLEVEL"
+])
+
+module.exports = levelMeUp
